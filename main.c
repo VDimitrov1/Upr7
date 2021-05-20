6f602f3c66f5e09a9169309e2a4b82266013dda2
Vladko <dimitrov@tu-sofia.bg>
2021-05-20 18:27:23 +0300

    V1

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <sys/ioctl.h>
 
 int main(int argc,char** argv)
 {
-	printf("Hello World");
+	struct winsize argp;
+	argp.ws_row=40;
+	argp.ws_col=164;
+
+	if(ioctl(0,TIOCGWINSZ,&argp)==0)
+	{
+		printf("Terminal lenght is %d\n",argp.ws_row);
+		printf("Terminal width is %d", argp.ws_col);
+	}
+	else
+	{
+		printf("Error using ioctl\n");
+		return EXIT_FAILURE;	
+	}
 	return EXIT_SUCCESS;
 }
